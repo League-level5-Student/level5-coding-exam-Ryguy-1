167531df2c24e684aef3eb4a137c898f967c4c28
League Student <league@iMac.attlocal.net>
2020-02-08 17:20:49 -0800

    Finished Both

--- src/Coding_Exam_A/CodingExamA.java
+++ src/Coding_Exam_A/CodingExamA.java
@@ -21,6 +21,37 @@ public class CodingExamA {
 		 * 
 		 * See the Coding_Exam_A_Demo.jar for an example of what the finished product should look like.
 		 */
-
+		String numBots = JOptionPane.showInputDialog("How many robots?");
+		String rValue = JOptionPane.showInputDialog("What r Value??");
+		String gValue = JOptionPane.showInputDialog("What g Value???");
+		String bValue = JOptionPane.showInputDialog("What b Value??");
+		String sidesShapes = JOptionPane.showInputDialog("How many sides per shape???");
+		
+		int botNum = Integer.parseInt(numBots);
+		int sidesNum = Integer.parseInt(sidesShapes);
+		
+		Robot[] bots = new Robot[botNum];
+		
+		for (int i = 0; i < botNum; i++) {
+			bots[i] = new Robot();
+		}
+		
+		int counter = 0;
+		int threadCounter = 0;
+		for (Robot r: bots) {
+			counter+=150;
+			r.penDown();
+			r.setSpeed(10);
+			r.setX(counter);
+			Thread thread = new Thread(() ->  {
+				for (int i = 0; i < sidesNum; i++) {
+					r.move(40);
+					r.turn(360/sidesNum);
+				}
+			});
+			thread.start();
+			
+			}
+		}
 	}
-}
+
--- src/Coding_Exam_B/CodingExamB.java
+++ src/Coding_Exam_B/CodingExamB.java
@@ -15,7 +15,7 @@ public class CodingExamB {
 	 */
 	
 	
-	public static String getLoggingInfo(String fileName) {
+	public static String getLoggingInfo(String fileName, boolean Addline) throws IOException {
 		/*
 		 * 1. Complete the getLoggingInfoMethod.
 		 *    The method takes in a String for a file and returns a String. 
@@ -24,19 +24,63 @@ public class CodingExamB {
 		 *    into one large String. The string will also state the file name and
 		 *    the line number for where each TODO was found. 
 		*/
+		String finalName = "";
 		
-		return "";
+		if (Addline == true) {
+			finalName =   "File: " + fileName;
+		}else{
+			finalName =  "\n" + "File: " + fileName;
+		}
+		
+		try {
+			BufferedReader bf = new BufferedReader(new FileReader(fileName));		
+			int trueFalse = 1;
+			int lineNum = 0;
+			while(trueFalse != 0) {
+				String line = bf.readLine();
+				lineNum+=1;
+				if(line == null) {
+					trueFalse = 0;
+				}else {
+					if(line.contains("TODO") && lineNum>10) {
+						finalName+= "\n" +lineNum + ":"+ "   " + line.trim();
+					}else if(line.contains("TODO")){
+						finalName+= "\n" +lineNum + ":"+ "    " + line.trim();
+					}
+							
+				}
+			}
+			bf.close();
+			return finalName + "\n";
+			
+			
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return finalName + "\n";
+		
+		
 	}
 	
-	public static void main(String[] args) {
-		String finalLogString = getLoggingInfo("src/Coding_Exam_B/classes/Camera.java");
-		finalLogString += getLoggingInfo("src/Coding_Exam_B/classes/RayTracedImageViewer.java");
-		finalLogString += getLoggingInfo("src/Coding_Exam_B/classes/RayTracer.java");
-		finalLogString += getLoggingInfo("src/Coding_Exam_B/classes/Vector3.java");
+	public static void main(String[] args) throws IOException {
+		String finalLogString = getLoggingInfo("src/Coding_Exam_B/classes/Camera.java", true);
+		finalLogString += getLoggingInfo("src/Coding_Exam_B/classes/RayTracedImageViewer.java", false);
+		finalLogString += getLoggingInfo("src/Coding_Exam_B/classes/RayTracer.java", false);
+		finalLogString += getLoggingInfo("src/Coding_Exam_B/classes/Vector3.java", false);
 		
 		/*
 		 * 2. Write the finalLogString to a file called TODO_Log.txt. The file should match TODO_Log_example.txt. 
 		 */
-
+		System.out.println(finalLogString);
+		
+		try {
+			FileWriter fr = new FileWriter("src/Coding_Exam_B/TODO_Log.txt");
+			fr.write(finalLogString);
+			fr.close();
+		} catch (Exception e) {
+			System.out.println("Write broke");
+		}
+		
 	}
 }
